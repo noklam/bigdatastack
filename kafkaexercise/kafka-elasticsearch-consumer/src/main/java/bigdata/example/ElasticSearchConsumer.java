4957d6bd6353f255bfb490d7468c2b47fa4155b4
noklam <channoklam@yahoo.com.hk>
2019-04-20 16:14:30 +0800

    Read from Kafka Consumer and write to ElasticSearch

--- kafkaexercise/kafka-elasticsearch-consumer/src/main/java/bigdata/example/ElasticSearchConsumer.java
+++ kafkaexercise/kafka-elasticsearch-consumer/src/main/java/bigdata/example/ElasticSearchConsumer.java
@@ -6,6 +6,11 @@ import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RequestOptions;
@@ -17,11 +22,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
 
 public class ElasticSearchConsumer {
     public static RestHighLevelClient createClient() {
-        String hostname = "https://JpzEa2S3GD:nLhtFsKpMcHRoY6q4yDZu@twitter-kafka-9841934087" +
-                ".ap-southeast-2.bonsaisearch.net";
+        String hostname = "twitter-kafka-9841934087.ap-southeast-2.bonsaisearch.net";
         String username = "JpzEa2S3GD";
         String password = "nLhtFsKpMcHRoY6q4yDZu";
 
@@ -40,24 +47,64 @@ public class ElasticSearchConsumer {
         return client;
     }
 
+    public static KafkaConsumer<String, String> createConsumer(String topic){
+
+        Properties properties = new Properties();
+        String bootstrapServers = "127.0.0.1:9092";
+        String groupId = "kafka-elasticsearch";
+
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+                StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG
+                , StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,
+                "earliest");
+
+        // create consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+        consumer.subscribe(Arrays.asList(topic));
+
+        return consumer;
+    }
+
 
     public static void main(String[] args) throws IOException {
         // Logger
         Logger logger = LoggerFactory.getLogger(ElasticSearchConsumer.class.getName());
         RestHighLevelClient client = createClient();
 
-        String jsonString = "{ \"foo\": \"bar\" }";
 
-        IndexRequest indexRequest = new IndexRequest(
-                "twitter", "tweets"
-        ).source(jsonString, XContentType.JSON);
 
-        IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
-        String id = indexResponse.getId();
-        logger.info(id);
+
+        KafkaConsumer<String, String> consumer = createConsumer("twitter_tweets");
+        while(true){
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+
+            for (ConsumerRecord<String, String> record:records) {
+                 // where we insert data into ElasticSearch
+                String jsonString = record.value();
+//                String jsonString = "{ \"foo\": \"bar\" }";
+
+                IndexRequest indexRequest = new IndexRequest(
+                        "twitter", "tweets"
+                ).source(jsonString, XContentType.JSON);
+
+                IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
+                String id = indexResponse.getId();
+                logger.info(id);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
         // close the client
-        client.close();
+//        client.close();
 
     }
 }
